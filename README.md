5e047baa101c4d034f7d83867d3d6ab4390edf5e
Steve Tomi <89354038+SteffQing@users.noreply.github.com>
2023-12-23 02:54:29 +0100

    Change PhishFort to OpSecurity

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PhishFort | Protect
+# OpSecurity | Protect
 
 Protect yourself from phishing attacks.
 
@@ -6,14 +6,14 @@ Protect yourself from phishing attacks.
 -----------------
 - 🌐Get notified when you are on a dangerous website. 
 - ✅See verified Twitter crypto-users. 
-- 👉Report dangerous websites, Twitter users, and mobile apps directly to the PhishFort team.
+- 👉Report dangerous websites, Twitter users, and mobile apps directly to the OpSecurity team.
 
 Why use Protect?
 -----------------
 
 - 🕶 Privacy focused - Your URLs are never sent to us, keep your browsing private.
 - 🔎 Open-source - Know what code is running in your browser (https://github.com/phishfort/protect/) 
-- 💸 Free! - Protect is a community project maintained by PhishFort. 
+- 💸 Free! - Protect is a community project maintained by OpSecurity. 
 - 🔮 Great coverage - We protect some of the largest crypto companies in the space, including MyEtherWallet, IDEX, Paxful. Our anti-phishing intel is fed directly to our plugin and is the most reliable way of staying safe.
 
 ♦ Status Indicators ♦
@@ -21,7 +21,7 @@ Why use Protect?
 
 The plugin will indicate different colors depending on the website you're visiting.
 
-- 💚 Green: Indicates that the domain is known and likely safe. For example, if you visit myetherwallet.com, the PhishFort Status Icon will glow blue. 
+- 💚 Green: Indicates that the domain is known and likely safe. For example, if you visit myetherwallet.com, the OpSecurity Status Icon will glow blue. 
 - ⚪  Grey: The site hasn't been categorized. It's not necessarily unsafe, but proceed with caution! 
 - 🔴 Red: The site is most likely dangerous. If you visit myWetherwallet.com (a typosquatting attack), the plugin will notify you that you are on an unsafe site and you'll be sent back to safety. You're also able to bypass the warning if you need!
 
--- js/twitter.js
+++ js/twitter.js
@@ -1,19 +1,23 @@
 var whitelist = [];
 
-console.log("Running Twitter monitoring")
+console.log("Running Twitter monitoring");
 
 function addBadges() {
-  let usernames = contains('span', '@');
+  let usernames = contains("span", "@");
 
   for (user of usernames) {
     let username = user.textContent.toLowerCase().substring(1);
-    if (whitelist.some(item => item.toLowerCase() === username)) {
-      if (!user.getAttribute("phishfort-tagged") && !user.parentElement.getAttribute("phishfort-tagged")) {
+    if (whitelist.some((item) => item.toLowerCase() === username)) {
+      if (
+        !user.getAttribute("phishfort-tagged") &&
+        !user.parentElement.getAttribute("phishfort-tagged")
+      ) {
         var icon = document.createElement("img");
-        icon.src = chrome.runtime.getURL('/img/twitter-whitelisted.png');
-        icon.style = "padding-left:3px;display:inline;vertical-align:text-bottom;float:none;height:15px;width:15px;left:15px;";
-        icon.title = `@${username} is a PhishFort verified user`;
-        icon.setAttribute('phishfort-badge', true);
+        icon.src = chrome.runtime.getURL("/img/twitter-whitelisted.png");
+        icon.style =
+          "padding-left:3px;display:inline;vertical-align:text-bottom;float:none;height:15px;width:15px;left:15px;";
+        icon.title = `@${username} is a OpSecurity verified user`;
+        icon.setAttribute("phishfort-badge", true);
         user.appendChild(icon);
         user.setAttribute("phishfort-tagged", true);
       }
@@ -21,7 +25,6 @@ function addBadges() {
   }
   removeReplyToBadges();
 }
-
 
 function removeReplyToBadges() {
   let links = document.links;
@@ -32,10 +35,10 @@ function removeReplyToBadges() {
         let img = link.childNodes[0].childNodes[0].childNodes[0].childNodes[1];
         let img2 = link.childNodes[0].childNodes[0].childNodes[1];
 
-        if (img.getAttribute('phishfort-badge')) {
+        if (img.getAttribute("phishfort-badge")) {
           img.setAttribute("style", "display:none");
         }
-        if (img2.getAttribute('phishfort-badge')) {
+        if (img2.getAttribute("phishfort-badge")) {
           img2.setAttribute("style", "display:none");
         }
       } catch (error) {
@@ -45,10 +48,10 @@ function removeReplyToBadges() {
           let img = link.childNodes[0].childNodes[2];
           let img2 = link.childNodes[0].childNodes[3];
 
-          if (img.getAttribute('phishfort-badge')) {
+          if (img.getAttribute("phishfort-badge")) {
             img.setAttribute("style", "display:none");
           }
-          if (img2.getAttribute('phishfort-badge')) {
+          if (img2.getAttribute("phishfort-badge")) {
             img2.setAttribute("style", "display:none");
           }
         } catch (error) {
@@ -67,15 +70,16 @@ function contains(selector, text) {
 }
 
 function setupObserver() {
-  let MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+  let MutationObserver =
+      window.MutationObserver || window.WebKitMutationObserver,
     eventListenerSupported = window.addEventListener;
 
   if (MutationObserver) {
-    let target = document.getElementsByTagName('body')[0];
+    let target = document.getElementsByTagName("body")[0];
 
     let config = {
       childList: true,
-      subtree: true
+      subtree: true,
     };
 
     let observer = new MutationObserver(function (mutations) {
@@ -85,12 +89,10 @@ function setupObserver() {
     });
 
     observer.observe(target, config);
-  }
-
-  else if (eventListenerSupported) {
-    let obj = document.getElementsByTagName('body')[0];
-    obj.addEventListener('DOMNodeInserted', addBadges, false);
-    obj.addEventListener('DOMNodeRemoved', addBadges, false);
+  } else if (eventListenerSupported) {
+    let obj = document.getElementsByTagName("body")[0];
+    obj.addEventListener("DOMNodeInserted", addBadges, false);
+    obj.addEventListener("DOMNodeRemoved", addBadges, false);
   }
 }
 
@@ -108,15 +110,14 @@ function getTwitterWhitelist() {
   });
 }
 
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   switch (request.func) {
     case "clearTagged":
-      let tags = document.querySelectorAll('[phishfort-tagged]');
+      let tags = document.querySelectorAll("[phishfort-tagged]");
       for (tag of tags) {
         tag.removeAttribute("phishfort-tagged");
       }
-      let existingBadges = document.querySelectorAll('[phishfort-badge]');
+      let existingBadges = document.querySelectorAll("[phishfort-badge]");
       for (badge of existingBadges) {
         badge.setAttribute("style", "display:none");
       }
