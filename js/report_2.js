4514b8a887b1fd2ac60f7207bd3c089797de8562
Steve Tomi <steveola23@gmail.com>
2024-03-15 14:17:57 +0100

    cleaning images on tutorial section

--- js/report_2.js
+++ js/report_2.js
@@ -2,23 +2,6 @@ let reportUrl =
   "https://safebrowsing.google.com/safebrowsing/report_phish/?hl=en&url=";
 let maliciousSite = "";
 
-browser.tabs.query(
-  {
-    currentWindow: true,
-    active: true,
-  },
-  function (tabs) {
-    maliciousSite = tabs[0].url;
-  }
-);
-
-// Listen for messages from content scripts
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.url) {
-    maliciousSite = message.url;
-  }
-});
-
 let link = document.getElementById("report");
 
 link.href = `${reportUrl}${maliciousSite}`;
